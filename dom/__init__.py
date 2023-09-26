1cb6402c98e2e626d620d52f467005bf3db28429
starwort <tcphone93@gmail.com>
2023-09-26 22:00:14 +0100

    Export short names by default as well

--- dom/__init__.py
+++ dom/__init__.py
@@ -1,3 +1,4 @@
+import typing
 from . import (
     base_classes,
     components,
@@ -13,5 +14,46 @@ from . import (
     tables,
 )
 from .plain_elements import *
+from .short import *
+
+
+def custom(
+    tag_name: str,
+    void: bool = True,
+) -> typing.Type[Element]:
+    """Create an ad-hoc Element class. Good for using web components."""
+    if void:
+
+        class CustomVoid(Void):
+            tag = tag_name
+
+            def __repr__(self) -> str:
+                return (
+                    "custom("
+                    + repr(self.tag)
+                    + ")("
+                    + ", ".join(f"{k}={v}" for k, v in self.attributes.items())
+                    + ")"
+                )
+
+        return CustomVoid
+    else:
+
+        class CustomContainer(Container):
+            tag = tag_name
+
+            def __repr__(self) -> str:
+                return (
+                    "custom("
+                    + repr(self.tag)
+                    + ")("
+                    + ", ".join(repr(child) for child in self.content)
+                    + (", " if self.content and self.attributes else "")
+                    + ", ".join(f"{k}={v!r}" for k, v in self.attributes.items())
+                    + ")"
+                )
+
+        return CustomContainer
+
 
 __version__ = "0.2.1a"
