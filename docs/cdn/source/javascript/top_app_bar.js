83c2e392a9bdc9a8208b34e3571e9a16fae94013
starwort <tcphone93@gmail.com>
2023-09-26 21:45:33 +0100

    Remove incomplete MDC support

--- docs/cdn/source/javascript/top_app_bar.js
+++
@@ -1,5 +0,0 @@
-import { MDCTopAppBar } from '@material/top-app-bar';
-
-// Instantiation
-let top_app_bar = document.querySelector('.mdc-top-app-bar');
-top_app_bar.mdc_top_app_bar = new MDCTopAppBar(top_app_bar);
--- prepare_cdn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in $(ls docs/cdn/source/styles/); do
-    sass docs/cdn/source/styles/$file docs/cdn/styles/${file%.scss}.css --style=compressed --no-source-map
-done
-webpack
